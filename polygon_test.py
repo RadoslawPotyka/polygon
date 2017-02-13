b5afc5ad0eeab7581830a1e05d8e5fea302c4ef6
Radek Potyka <rpotyka@milnerbrowne.com>
2017-02-13 19:40:22 +0100

    Change polygon test structure. Add file ith results and test data printing

--- polygon_test.py
+++ polygon_test.py
@@ -1,7 +1,13 @@
 import polygon
 
 def polygon_test():
-	test_data = [3.0, 2.1, 6.5, 7.7]
+	test_data1 = [(1.0, 1.0), (4.0, 1.0), (1.0, 4.0), (4.0, 4.0)]
+	test_data2 = [(1.0, 2.0), (3.0, 4.0), (2.0, 5.0)]
 
-	print(polygon(test_data))
-	print("Test successfull ...")
+	with open("test_results.txt", "w") as test_file:
+		result1 = polygon(test_data1)
+		result2 = polygon(test_data2)
+		text = "Test data 1 {0} \n Result: {1} \n test data 2 {3} \n Result: {4}".format(test_data1, result1, test_data2, result2)
+
+		test_file.write(text)
+		print("Test successfull ...")
